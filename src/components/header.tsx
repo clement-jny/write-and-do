e8a4f27d3efdbc31a7187f470fe499dd03d1a99d
clement-jny <clementjaunay@free.fr>
2024-11-26 18:08:42 +0100

    chore: rename nav-bar to header

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+import { CustomSidebarTrigger } from "./custom-sidebar-trigger";
+
+export const Header = () => {
+  return (
+    <header className="w-[97%] h-24 mx-auto mt-7 bg-app-background-primary rounded-xl flex items-center drop-shadow-xl sticky top-7 gap-2 shrink-0 px-4">
+      <CustomSidebarTrigger />
+
+      {/* <Breadcrumb>
+					<BreadcrumbList>
+					<BreadcrumbItem>
+						<BreadcrumbPage>October 2024</BreadcrumbPage>
+					</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb> */}
+
+      <h1 className="font-medium text-4xl ml-5 border-b-2 border-app-primary">
+        Welcome in Write & Do
+      </h1>
+    </header>
+  );
+};
