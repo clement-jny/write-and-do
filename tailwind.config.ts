c70f12e98ef86f9aa985ce74f4ba8628a67f5120
clement-jny <clementjaunay@free.fr>
2024-11-27 12:05:13 +0100

    chore: indent

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -49,28 +49,28 @@ export default {
           "3": "hsl(var(--chart-3))",
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
-  			},
-  			sidebar: {
-  				DEFAULT: 'hsl(var(--sidebar-background))',
-  				foreground: 'hsl(var(--sidebar-foreground))',
-  				primary: 'hsl(var(--sidebar-primary))',
-  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-  				accent: 'hsl(var(--sidebar-accent))',
-  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				border: 'hsl(var(--sidebar-border))',
-  				ring: 'hsl(var(--sidebar-ring))'
-  			},
+		},
+		sidebar: {
+			DEFAULT: 'hsl(var(--sidebar-background))',
+			foreground: 'hsl(var(--sidebar-foreground))',
+			primary: 'hsl(var(--sidebar-primary))',
+			'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+			accent: 'hsl(var(--sidebar-accent))',
+			'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+			border: 'hsl(var(--sidebar-border))',
+			ring: 'hsl(var(--sidebar-ring))'
+		},
 
-			app: {
-				primary: 'var(--app-primary)',
-				secondary: 'var(--app-secondary)',
-				ellipse1: 'var(--app-ellipse-1)',
-				ellipse2: 'var(--app-ellipse-2)',
-				background: {
-					primary: 'var(--app-background-primary)',
-					secondary: 'var(--app-background-secondary)'
-				}
-      },
+		app: {
+			primary: 'var(--app-primary)',
+			secondary: 'var(--app-secondary)',
+			ellipse1: 'var(--app-ellipse-1)',
+			ellipse2: 'var(--app-ellipse-2)',
+			background: {
+				primary: 'var(--app-background-primary)',
+				secondary: 'var(--app-background-secondary)'
+			}
+		},
       },
       borderRadius: {
         lg: "var(--radius)",
