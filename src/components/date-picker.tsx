f912305e9bd9c03bf42dca392a20b10db8ba77ea
clement-jny <clementjaunay@free.fr>
2024-11-27 11:49:10 +0100

    feat: range mode on calendar

--- src/components/date-picker.tsx
+++ src/components/date-picker.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { Calendar } from "@/components/ui/calendar";
 import { SidebarGroup, SidebarGroupContent } from "@/components/ui/sidebar";
+import { type DateRange } from "react-day-picker";
 
 export function DatePicker() {
+  const [selectedDates, setSelectedDates] = useState<DateRange | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    console.log(
+      "date",
+      `From ${selectedDates?.from?.toLocaleDateString()} to
+              ${selectedDates?.to?.toLocaleDateString()}`
+    );
+  }, [selectedDates]);
+
   return (
     <SidebarGroup className="px-0">
       <SidebarGroupContent>
-        <Calendar className="[&_[role=gridcell].bg-accent]:bg-sidebar-primary [&_[role=gridcell].bg-accent]:text-sidebar-primary-foreground [&_[role=gridcell]]:w-[33px]" />
+        <Calendar
+          mode="range"
+          max={4}
+          selected={selectedDates}
+          onSelect={setSelectedDates}
+          footer={
+            selectedDates &&
+            `From ${selectedDates?.from?.toLocaleDateString()} to
+              ${selectedDates?.to?.toLocaleDateString()}`
+          }
+        />
       </SidebarGroupContent>
     </SidebarGroup>
   );
