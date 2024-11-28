a36ac8e35d21476ddcf72eee1d0e292d0238009f
clement-jny <clementjaunay@free.fr>
2024-11-28 09:28:55 +0100

    feat: update from FirebaseUser to User

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,33 +1,55 @@
 "use client";
 
-import { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-import { onAuthStateChanged, User as FirebaseUser } from 'firebase/auth';
-import { auth } from '../services/firebase';
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import { onAuthStateChanged, User as FirebaseUser } from "firebase/auth";
+import { auth } from "../services/firebase";
+import { User } from "@/models/User";
+import { getUserInfos } from "@/hooks/useUser";
 
 interface UserContextProps {
-    user: FirebaseUser | null;
+  user: User | null;
 }
 
 const UserContext = createContext<UserContextProps>({ user: null });
 
 export const UserProvider = ({ children }: { children: ReactNode }) => {
-    const [user, setUser] = useState<FirebaseUser | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setUser(user);
-        });
+  const getUser = async (userUid: string) => {
+    const users = await getUserInfos(userUid);
 
-        return () => unsubscribe();
-    }, []);
+    setUser({
+      uid: users[0].uid,
+      email: users[0].email,
+      lastname: users[0].lastname,
+      firstname: users[0].firstname,
+    });
+  };
 
-    return (
-        <UserContext.Provider value={{ user }}>
-            {children}
-        </UserContext.Provider>
-    );
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (!firebaseUser) {
+        setUser(null);
+        return;
+      } else {
+        getUser(firebaseUser.uid);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
 };
 
 export const useUser = () => {
-    return useContext(UserContext);
+  return useContext(UserContext);
 };
